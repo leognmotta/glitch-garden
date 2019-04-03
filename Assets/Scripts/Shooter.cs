61bc9280dd5e79a043b12bca8a5be9baba489104
Leonardo <leo@domboscoatacado.com.br>
2019-04-03 08:37:14 -0300

    Check if there is enemy in lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,6 +6,39 @@ public class Shooter : MonoBehaviour {
 
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
+    AttackerSpawner myLaneSpawner;
+
+    private void Start() {
+        SetLaneSpawner();
+    }
+
+    private void Update() {
+        if (IsAttackerInLane()) {
+            Debug.Log("Shoot");
+        } else {
+            Debug.Log("Wait");
+        }
+    }
+
+    private void SetLaneSpawner() {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners) {
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if (isCloseEnough) {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane() {
+        if (myLaneSpawner.transform.childCount <= 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
     public void Fire() {
         Instantiate(projectile, gun.transform.position, transform.rotation);
