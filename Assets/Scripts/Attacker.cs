355dbfc662b9e69349a03ba54d4783bb6477a501
Leonardo <leo@domboscoatacado.com.br>
2019-04-08 13:48:40 -0300

    Timer

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -8,6 +8,14 @@ public class Attacker : MonoBehaviour {
     float currentSpeed = 1f;
     GameObject currentTarget;
 
+    private void Awake() {
+        FindObjectOfType<LevelController>().AttackerSpawned();
+    }
+
+    private void OnDestroy() {
+        FindObjectOfType<LevelController>().AttackerDestroyed();
+    }
+
     // Update is called once per frame
     void Update() {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
